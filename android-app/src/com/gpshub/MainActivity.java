0b3e003aa793b8905093cb92f9c7e8ee61715091
illusionww <illusionww@gmail.com>
2014-06-29 00:24:06 +0400

    gps works

--- android-app/src/com/gpshub/MainActivity.java
+++ android-app/src/com/gpshub/MainActivity.java
@@ -1,9 +1,15 @@
 package com.gpshub;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
-import android.widget.CompoundButton;
-import android.widget.Switch;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -22,52 +28,73 @@ import java.util.TimerTask;
 public class MainActivity extends Activity {
 
     Timer timer;
+    public static boolean enabled = false;
+    private android.content.Context mContext = this;
+    private GPSMonitor gps;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Switch s = (Switch) findViewById(R.id.onoff);
+        final Button button = (Button) findViewById(R.id.onoff);
+        gps = new GPSMonitor(MainActivity.this);
 
-        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
-                if (isChecked) {
-                    timer = new Timer(true);
-                    final GPSTracker gps = new GPSTracker(MainActivity.this);
-
-                    if (gps.canGetLocation()) {
-
-                        TimerTask timerTask = new TimerTask() {
-                            @Override
-                            public void run() {
-                                double latitude = gps.getLatitude();
-                                double longitude = gps.getLongitude();
-
-                                try {
-                                    System.out.println("lat: " + latitude + ", lng: " + longitude);
-                                    postData(latitude, longitude);
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        };
-                        timer.scheduleAtFixedRate(timerTask, 0, 2 * 1000);
-
+            public void onClick(View view) {
+                if (!enabled) {
+                    if (gps.isGPSEnabled()) {
+                        startTimer();
+                        button.setText("disable tracker");
                     } else {
-                        gps.showSettingsAlert();
+                        showSettingsAlert();
+                        return;
                     }
                 } else {
-                    timer.cancel();
+                    stopTimer();
+                    button.setText("enable tracker");
                 }
+
+                enabled = !enabled;
             }
         });
     }
 
+    public void startTimer() {
+        gps.start();
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Location location = gps.getCurrentLocation();
+                if (location == null) {
+                    System.out.println("last known");
+                    location = gps.getLastKnownLocation();
+                }
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                try {
+                    postData(latitude, longitude);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, 2 * 1000);
+    }
+
+    public void stopTimer() {
+        timer.cancel();
+        gps.stop();
+    }
+
     public void postData(double lat, double lng) throws IOException {
+        System.out.println("lat: " + lat + ", lng: " + lng);
+
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("http://javafiddle.org/gpsHub/actions/drivers.php");
 
@@ -84,6 +111,34 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
     }
 
 }
