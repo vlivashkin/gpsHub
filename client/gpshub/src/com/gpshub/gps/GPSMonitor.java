3e7dae03fd0c38b4e7d93fac0bc88ae1f3e9a9f8
illusionww <illusionww@gmail.com>
2014-07-09 19:07:23 +0400

    some changes in GPS module

--- client/gpshub/src/com/gpshub/gps/GPSMonitor.java
+++ client/gpshub/src/com/gpshub/gps/GPSMonitor.java
@@ -7,32 +7,24 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class GPSMonitor {
 
     private static final String TAG = GPSMonitor.class.getSimpleName();
-    private static final long MIN_TIME_BW_UPDATES = 1000;  // 1 sec;
-    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 2;  // 2 meters
-    private final Context context;
+    private static final long GPS_UPDATE_TIME = 1000;  // 1 sec;
+    private static final float GPS_UPDATE_DISTANCE = 2;  // 2 meters
     private final LocationManager locationManager;
-    private LocationListener listener;
     private boolean collecting = false;
+    Context context;
 
     private Location currentLocation = null;
     private final LocationListener _listener = new Listener();
 
-    public GPSMonitor(Context context) {
-        this(context, null);
-    }
 
-    public GPSMonitor(Context context, LocationListener listener) {
-        this.listener = listener;
+    public GPSMonitor(Context context) {
         this.context = context;
         locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-    }
-
-    public boolean isGPSEnabled() {
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 
     public static boolean isGPSEnabled(Activity activity) {
@@ -40,28 +32,8 @@ public class GPSMonitor {
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 
-    public Location getLastKnownLocation() {
-        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-    }
-
-    public Context getContext() {
-        return context;
-    }
-
     public Location getCurrentLocation() {
         return currentLocation;
-    }
-
-    public LocationListener getListener() {
-        return listener;
-    }
-
-    public void setListener(LocationListener listener) {
-        this.listener = listener;
-    }
-
-    public boolean isCollecting() {
-        return collecting;
     }
 
     public void start() {
@@ -69,12 +41,12 @@ public class GPSMonitor {
             return;
         locationManager.requestLocationUpdates(
                 LocationManager.GPS_PROVIDER,
-                MIN_TIME_BW_UPDATES,
-                MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                GPS_UPDATE_TIME,
+                GPS_UPDATE_DISTANCE,
                 _listener
         );
         collecting = true;
-        currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        currentLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         System.out.println("GPSMonitor started.");
     }
 
@@ -88,37 +60,23 @@ public class GPSMonitor {
 
         @Override
         public void onLocationChanged(Location location) {
-            if (listener != null)
-                listener.onLocationChanged(location);
             Log.d(TAG, "onLocationChanged(): lat="+location.getLatitude() + ", long=" + location.getLongitude());
-            if (location.hasSpeed())
-                Log.d(TAG, "onLocationChanged(): speed=" + location.getSpeed());
-            if (location.hasBearing())
-                Log.d(TAG, "onLocationChanged(): bearing=" + location.getBearing());
-            if (location.hasAccuracy())
-                Log.d(TAG, "onLocationChanged():  accuracy=" + location.getAccuracy());
-            if (location.hasAltitude())
-                Log.d(TAG, "onLocationChanged(): alt="+location.getAltitude());
-
             currentLocation = location;
         }
 
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
-            if (listener != null)
-                listener.onStatusChanged(provider, status, extras);
+
         }
 
         @Override
         public void onProviderEnabled(String provider) {
-            if (listener != null)
-                listener.onProviderEnabled(provider);
+            Toast.makeText(context, "Gps turned on", Toast.LENGTH_LONG).show();
         }
 
         @Override
         public void onProviderDisabled(String provider) {
-            if (listener != null)
-                listener.onProviderDisabled(provider);
+            Toast.makeText(context, "Gps turned off", Toast.LENGTH_LONG).show();
         }
     }
 }
